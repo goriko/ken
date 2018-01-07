01a8fed0d8668a875516379014346bf2c6163399
goriko <guanzonedrian@gmail.com>
2018-01-08 00:10:28 +0800

    added files

---
+++ register.php
@@ -0,0 +1,41 @@
+<html>
+  <head>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+  </head>
+  <body>
+    <br>
+    <div class="col-md-offset-2 col-md-8">
+      <form>
+        <div class="form-row">
+          <div class="form-group col-md-6">
+            <label>Email</label>
+            <input type="email" class="form-control" placeholder="Email">
+          </div>
+          <div class="form-group col-md-6">
+            <label>Password</label>
+            <input type="password" class="form-control" placeholder="Password">
+          </div>
+        </div>
+        <div class="form-group">
+          <label>Address</label>
+          <input type="text" class="form-control" placeholder="1234 Main St">
+        </div>
+        <div class="form-row">
+          <div class="form-group col-md-6">
+            <label>City</label>
+            <input type="text" class="form-control">
+          </div>
+          <div class="form-group col-md-4">
+            <label>State</label>
+            <input type="text" class="form-control">
+          </div>
+          <div class="form-group col-md-2">
+            <label>Zip</label>
+            <input type="text" class="form-control">
+          </div>
+        </div>
+        <button type="submit" class="btn btn-primary">Register</button>
+      </form>
+    </div>
+  </body>
+</html>
